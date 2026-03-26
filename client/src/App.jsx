e62270e8c41ae239e3ca0bdd4337fac99e7bedf5
Sajjad9908 <gulbadan128@gmail.com>
2026-03-26 15:51:22 +0500

    set protected Route for

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,7 +65,7 @@ const router=createBrowserRouter([
   },
   {
     path:'/products',
-    element:<><Navbar/><Products/><Footer/></>
+    element:<ProtectedRoute><Navbar/><Products/><Footer/></ProtectedRoute>
   },
   {path:'/product/:id',
     element:<><Navbar/> <SingleProduct/> </> 
